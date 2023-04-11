5f8aadc36dd19eb607ccd11c6f457df0e372d4f4
Svyatoslav <79059242415@yandex.ru>
2023-04-12 02:05:36 +0700

    deleted custom fkeys

--- server/models/groups/groups_has_group_document.model.js
+++ server/models/groups/groups_has_group_document.model.js
@@ -1,19 +1,5 @@
 export default (sequelize, Sequelize) => {
     const groupsHasgroupDocumentModel = sequelize.define("groups_has_group_document", {
-        group_id: {
-            type: Sequelize.INTEGER,
-            references: {
-                model: groupModel,
-                key: 'id'
-            }
-        },
-        group_document_id: {
-            type: Sequelize.INTEGER,
-            references: {
-                model: groupDocumentModel,
-                key: 'id'
-            }
-        }
     });
     
     return groupsHasgroupDocumentModel;
--- server/models/learners/learners_has_learner_document.model.js
+++ server/models/learners/learners_has_learner_document.model.js
@@ -1,19 +1,5 @@
 export default (sequelize, Sequelize) => {
     const learnersHasLearnerDocumentModel = sequelize.define("learners_has_learner_document", {
-        learner_id: {
-            type: Sequelize.INTEGER,
-            references: {
-                model: learnerModel,
-                key: 'id'
-            }
-        },
-        learner_document_id: {
-            type: Sequelize.INTEGER,
-            references: {
-                model: learnerDocumentModel,
-                key: 'id'
-            }
-        }
     });
     
     return learnersHasLearnerDocumentModel;
